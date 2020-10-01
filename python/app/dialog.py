1dc4b17b1591f3d1c51d23d414473f482d84f578
jkiser <joseph.kiser@flightschoolstudio.com>
2020-09-30 23:39:16 -0500

    Finalized tool logic.

--- python/app/dialog.py
+++ python/app/dialog.py
@@ -1,43 +1,55 @@
-# Copyright (c) 2013 Shotgun Software Inc.
-#
-# CONFIDENTIAL AND PROPRIETARY
-#
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
-# Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
-# not expressly granted therein are reserved by Shotgun Software Inc.
+#!/usr/bin/env python
+#------------------------------------------------------------------------------#
+#-------------------------------------------------------------------- HEADER --#
+"""
+Copyright Project Flight School, LLC, 2008-2019 - All rights reserved.
+This software is the property of Project Flight School, LLC. It may not be 
+sold, transferred, assigned, licensed, copied, distributed, or otherwise 
+used without written permission.
 
+:author:
+    Joseph Kiser
+
+:description:
+    Tool to facilitate in the setup of vhd drive mounts for shotgun.
+"""
+
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------- IMPORTS --#
+# built-in
 import sgtk
 import os
 import sys
 import threading
+import distutils.spawn
 
-# by importing QT from sgtk rather than directly, we ensure that
-# the code will be compatible with both PySide and PyQt.
+# third-party
 from sgtk.platform.qt import QtCore, QtGui
 from .ui.dialog import Ui_Dialog
 from fs_perforce_tools import perforce_utils
 from sgtk.util import ShotgunPath
 
-# standard toolkit logger
-logger = sgtk.platform.get_logger(__name__)
+# external
+from fs_ui.dialogs.popup import show_error, show_message
+from fs_utils.file_system import join, set_permissions
 
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------- GLOBALS --#
+LOGGER = sgtk.platform.get_logger(__name__)
 VDH_DEPOT = 'VhdFiles'
 
+#------------------------------------------------------------------------------#
+#----------------------------------------------------------------- FUNCTIONS --#
 def show_dialog(app_instance):
     """
     Shows the main dialog window.
     """
-    # in order to handle UIs seamlessly, each toolkit engine has methods for launching
-    # different types of windows. By using these methods, your windows will be correctly
-    # decorated and handled in a consistent fashion by the system.
-
-    # we pass the dialog class to this method and leave the actual construction
-    # to be carried out by toolkit.
-    app_instance.engine.show_dialog('FS Mount Creator', app_instance, AppDialog)
+    app_instance.engine.show_dialog('FS Mount Creator', app_instance,
+                                     AppDialog)
 
 
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------ CLASSES ---#
 class AppDialog(QtGui.QWidget):
     """
     Main application dialog window
@@ -49,33 +61,26 @@ class AppDialog(QtGui.QWidget):
         """
         # first, call the base class and let it do its thing.
         QtGui.QWidget.__init__(self)
-        
+
         self.client_created = None
-        self.client_dict = dict()
-        
+        self.cli_dict = dict()
+
         # now load in the UI that was created in the UI designer
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-
-        self.populate_combo()
-
-        self.p4_fw = perforce_utils.get_p4_fw()
-
-        self.ui.create_btn.clicked.connect(self._create_mount_cb)
-
-        self.ui.close_btn.clicked.connect(self._close_cb)
         
+        self.powershell_loc = None
+        self.root_location = None
+        self.vhd_file_path = None
+        self.ps_file_path = None
+        
+        self.p4_fw = perforce_utils.get_p4_fw()
+        self.ui.create_btn.clicked.connect(self._create_mount_cb)
+        self.ui.close_btn.clicked.connect(self._close_cb)
         self.ui.vhd_location_browse_btn.clicked.connect(self._open_browser)
 
         self._app = sgtk.platform.current_bundle()
-    
-    def populate_combo(self):
-        """
-        This method checks perforce for the different size vhd file options.
-        """
-        self.ui.vhd_size_combo.clear()
-        self.ui.vhd_size_combo.addItem('test')
-
+        
     def _open_browser(self):
         """
         Method called to open the folder browser.
@@ -88,31 +93,41 @@ class AppDialog(QtGui.QWidget):
 
     def _create_mount_cb(self):
         """
+        Method called to create the actual mount.
         """
+        self.powershell_loc = distutils.spawn.find_executable('powershell')
+        if not self.powershell_loc:
+            msg = ('Windows powershell can not be located on your machine '
+                   'please contact a TD!')
+            show_error(msg, 'Error!')
+            return
+
         self.client_created = self._create_client()
         if not self.client_created:
             return
         self._create_mapping()
-        
         self._sync_files()
-
+        self._mount_vhd()
         self.ui.vhd_location_le.clear()
 
     def _create_client(self):
         """
+        Method to create a client to map the vhd file.
         """
-        root_location = str(self.ui.vhd_location_le.text())
-        if not root_location:
-            logger.debug('A Location for the VHD file has not been found. '
-                         'Please use the file browser to establish a '
-                         'location.')
+        self.root_location = str(self.ui.vhd_location_le.text())
+        if not self.root_location:
+            msg = ('A Location for the VHD file has not been found. Please '
+                   'use the file browser to establish a location.')
+            show_error(msg, 'Error!')
             return None
-        self.client_dict = self.p4_fw.utils.create_client_for_user(root=root_location)
+        self.cli_dict = self.p4_fw.utils.create_client_for_user(root=self.root_location)
 
-        return self.client_dict['Client']
+        return self.cli_dict['Client']
     
     def _create_mapping(self):
         """
+        Method to create a mapping for our created client so the vhd file 
+        gets synced locally.
         """
         mapping = '//{0}/... //{1}/{0}/...'.format(VDH_DEPOT, 
                                                    self.client_created)
@@ -120,12 +135,39 @@ class AppDialog(QtGui.QWidget):
     
     def _sync_files(self):
         """
+        Method to actually sync the vhd file.
         """
         p4_connection = self.p4_fw.connection.connect(None, None, 
                                                       client=self.client_created, 
                                                       skip_client=False)
-        p4_file_path = '//{0}/VhdFiles/shotgun_vhd_500gb.vhdx'.format(self.client_created)
-        self.p4_fw.utils.sync_files([p4_file_path], p4=p4_connection)
+        vhd_file_path = '//{0}/VhdFiles/shotgun_vhd_500gb.vhdx'.format(self.client_created)
+        ps_file_path = '//{0}/VhdFiles/mount_vhd.ps1'.format(self.client_created)
+        self.vhd_file_path = '{0}VhdFiles\shotgun_vhd_500gb.vhdx'.format(self.root_location)
+        self.ps_file_path = '{0}VhdFiles\mount_vhd.ps1'.format(self.root_location)
+        self.p4_fw.utils.sync_files([vhd_file_path, ps_file_path], p4=p4_connection)
+    
+    def _mount_vhd(self):
+        """
+        Method to mount the vhd file.
+        """
+        self.p4_fw.utils.delete_client(self.client_created, force=True)
+        
+        self.powershell_loc = self.powershell_loc.replace('\\', '/')
+        self.vhd_file_path = self.vhd_file_path.replace('\\', '/')
+        self.ps_file_path = self.ps_file_path.replace('\\', '/')
+        
+        set_permissions(self.vhd_file_path)
+        set_permissions(self.ps_file_path)
+
+        call = '{0} -ExecutionPolicy Bypass -File {1}'.format(self.powershell_loc, 
+                                                              self.ps_file_path)
+        os.system(call)
+        
+        msg = 'VHD file successfully mounted!'
+        show_message(msg, 'Success!')
         
     def _close_cb(self):
+        """
+        Method called to close the dialog.
+        """
         self.close()
--- python/app/ui/dialog.py
+++ python/app/ui/dialog.py
@@ -1,46 +1,54 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
+#------------------------------------------------------------------------------#
+#-------------------------------------------------------------------- HEADER --#
+"""
+Copyright Project Flight School, LLC, 2008-2019 - All rights reserved.
+This software is the property of Project Flight School, LLC. It may not be 
+sold, transferred, assigned, licensed, copied, distributed, or otherwise 
+used without written permission.
 
-################################################################################
-## Form generated from reading UI file 'dialog.ui'
-##
-## Created by: Qt User Interface Compiler version 5.14.2
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
+:author:
+    Joseph Kiser
+
+:description:
+    Form generated from reading UI file 'dialog.ui' 
+    Created by: Qt User Interface Compiler version 5.14.2
+    WARNING! All changes made in this file will be lost when recompiling 
+    UI file!
+"""
+
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------- IMPORTS --#
+# built-in
+
+# third-party
 from tank.platform.qt import QtCore, QtGui
 
+# external
 import resources_rc
 
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------- GLOBALS --#
+
+#------------------------------------------------------------------------------#
+#----------------------------------------------------------------- FUNCTIONS --#
+
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------ CLASSES ---#
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         if not Dialog.objectName():
             Dialog.setObjectName(u"Dialog")
-        Dialog.resize(380, 141)
+        Dialog.resize(350, 50)
+
         self.horizontalLayout = QtGui.QHBoxLayout(Dialog)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.vertical_layout = QtGui.QVBoxLayout()
         self.vertical_layout.setSpacing(6)
         self.vertical_layout.setObjectName(u"vertical_layout")
         self.vertical_layout.setSizeConstraint(QtGui.QLayout.SetDefaultConstraint)
-        self.vertical_layout.setContentsMargins(0, 0, 0, 0)
-        self.vhd_size_hor = QtGui.QHBoxLayout()
-        self.vhd_size_hor.setObjectName(u"vhd_size_hor")
-        self.vhd_size_lbl = QtGui.QLabel(Dialog)
-        self.vhd_size_lbl.setObjectName(u"vhd_size_lbl")
-        self.vhd_size_hor.addWidget(self.vhd_size_lbl)
-        self.horizontalSpacer = QtGui.QSpacerItem(25, 5, QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Minimum)
-        self.vhd_size_hor.addItem(self.horizontalSpacer)
-        self.vhd_size_combo = QtGui.QComboBox(Dialog)
-        self.vhd_size_combo.setObjectName(u"vhd_size_combo")
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(1)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.vhd_size_combo.sizePolicy().hasHeightForWidth())
-        self.vhd_size_combo.setSizePolicy(sizePolicy)
-        self.vhd_size_hor.addWidget(self.vhd_size_combo)
-        self.vertical_layout.addLayout(self.vhd_size_hor)
-        self.verticalSpacer_2 = QtGui.QSpacerItem(20, 3, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Fixed)
-        self.vertical_layout.addItem(self.verticalSpacer_2)
+        self.vertical_layout.setContentsMargins(0, 0, 0, 0)        
         self.vhd_location_hor = QtGui.QHBoxLayout()
         self.vhd_location_hor.setObjectName(u"vhd_location_hor")
         self.vhd_location_hor.setContentsMargins(0, 0, -1, -1)
@@ -75,11 +83,9 @@ class Ui_Dialog(object):
         self.horizontalLayout.addLayout(self.vertical_layout)
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
-    # setupUi
 
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "The Current Sgtk Environment", None, QtGui.QApplication.UnicodeUTF8))
-        self.vhd_size_lbl.setText(QtGui.QApplication.translate("Dialog", "VHD File Size: ", None, QtGui.QApplication.UnicodeUTF8))
         self.vhd_location_lbl.setText(QtGui.QApplication.translate("Dialog", "VHD File Location: ", None, QtGui.QApplication.UnicodeUTF8))
         self.vhd_location_browse_btn.setText(QtGui.QApplication.translate("Dialog", "Browse", None, QtGui.QApplication.UnicodeUTF8))
         self.create_btn.setText(QtGui.QApplication.translate("Dialog", "Create Mount", None, QtGui.QApplication.UnicodeUTF8))
